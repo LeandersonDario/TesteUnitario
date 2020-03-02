36df2f8ba9a65ec6c0981ef71d25b2bff75be993
LeandersonDario <38437048+LeandersonDario@users.noreply.github.com>
2020-03-02 13:36:43 -0300

    Update TestuFrmCalculaCustoMedio.pas

--- Test/GuiTesteFormulario/TestuFrmCalculaCustoMedio.pas
+++ Test/GuiTesteFormulario/TestuFrmCalculaCustoMedio.pas
@@ -1,12 +1,12 @@
 unit TestuFrmCalculaCustoMedio;
 {
- Desenvolvida por Dario projeto Piloto o princípio AAA – Arrange, Act e Assert.
+ Desenvolvida por Dario projeto Piloto o princÃ­pio AAA â€“ Arrange, Act e Assert.
 
  Principios
- Siga o princípio AAA – Arrange, Act e Assert.
- Arrange – preparar todas as condições necessárias para a execução do teste.
- Act – executar o que será testado.
- Assert – validar as informações depois do teste.
+ Siga o princÃ­pio AAA â€“ Arrange, Act e Assert.
+ Arrange â€“ preparar todas as condiÃ§Ãµes necessÃ¡rias para a execuÃ§Ã£o do teste.
+ Act â€“ executar o que serÃ¡ testado.
+ Assert â€“ validar as informaÃ§Ãµes depois do teste...
 }
 
 interface
@@ -34,7 +34,7 @@ type
     FfrmCalculaCusto: TfrmCalculaCusto;
     // formulario   calcula custo
     FedtQtde_Estoque_Anterior: TEdit;
-    FedtCusto_Médio_Anterior: TEdit;
+    FedtCusto_MÃ©dio_Anterior: TEdit;
     FedtQuantidade_comprada: TEdit;
     FedtVl_Reposicao: TEdit;
     FedtQtde_Anterior: TEdit;
@@ -74,27 +74,27 @@ begin
   ActionDelay := 1000;
   FedtQtde_Estoque_Anterior := FindControl('edtQtde_Estoque_Anterior') as TEdit;
   FedtQtde_Estoque_Anterior.Text := '10';
-  CheckEquals('10', FedtQtde_Estoque_Anterior.Text,'Teste de digitação edtQtde_Estoque_Anterior' );
+  CheckEquals('10', FedtQtde_Estoque_Anterior.Text,'Teste de digitaÃ§Ã£o edtQtde_Estoque_Anterior' );
 
-  FedtCusto_Médio_Anterior := FindControl('edtCusto_Médio_Anterior') as TEdit;
-  FedtCusto_Médio_Anterior.Text := '10';
-  CheckEquals('10', FedtCusto_Médio_Anterior.Text,'Teste de digitação edtCusto_Médio_Anterior.Text' );
+  FedtCusto_MÃ©dio_Anterior := FindControl('edtCusto_MÃ©dio_Anterior') as TEdit;
+  FedtCusto_MÃ©dio_Anterior.Text := '10';
+  CheckEquals('10', FedtCusto_MÃ©dio_Anterior.Text,'Teste de digitaÃ§Ã£o edtCusto_MÃ©dio_Anterior.Text' );
 
   FedtQuantidade_comprada := FindControl('edtQuantidade_comprada') as TEdit;
   FedtQuantidade_comprada.Text := '10';
-  CheckEquals('10', FedtQuantidade_comprada.Text,'Teste de digitação edtQuantidade_comprada' );
+  CheckEquals('10', FedtQuantidade_comprada.Text,'Teste de digitaÃ§Ã£o edtQuantidade_comprada' );
 
   FedtVl_Reposicao := FindControl('edtVl_Reposicao') as TEdit;
   FedtVl_Reposicao.Text := '10';
-  CheckEquals('10', FedtVl_Reposicao.Text, 'Teste de digitação edtVl_Reposicao');
+  CheckEquals('10', FedtVl_Reposicao.Text, 'Teste de digitaÃ§Ã£o edtVl_Reposicao');
 
   FedtQtde_Anterior := FindControl('edtQtde_Anterior') as TEdit;
   FedtQtde_Anterior.Text := '10';
-  CheckEquals('10', FedtQtde_Anterior.Text, 'Teste de digitação edtQtde_Anterior');
+  CheckEquals('10', FedtQtde_Anterior.Text, 'Teste de digitaÃ§Ã£o edtQtde_Anterior');
 
   FedtQtd_Comprada := FindControl('edtQtd_Comprada') as TEdit;
   EnterTextInto(FedtQtd_Comprada, '10');
-  Checkequals('10', FedtQtd_Comprada.Text, 'Teste de digitação edtQtd_Comprada');
+  Checkequals('10', FedtQtd_Comprada.Text, 'Teste de digitaÃ§Ã£o edtQtd_Comprada');
 
 end;
 
@@ -113,17 +113,17 @@ procedure TestTfrmCalculaCusto.Valida_SetFocus_Tela();
 begin
   FFailsOnNoChecksExecuted := false;
   SetFocus(FfrmCalculaCusto.btnCalcular);
-  Check(not IsFocused(FfrmCalculaCusto.btnCalcular), 'Teste com Falha, pois o SetFocus não está no btnCalcular');
-  Check(IsFocused(FfrmCalculaCusto.btnCalcular), ' Teste OK, pois verifica que  de fato o SetFocus não está no btnCalcular');
+  Check(not IsFocused(FfrmCalculaCusto.btnCalcular), 'Teste com Falha, pois o SetFocus nÃ£o estÃ¡ no btnCalcular');
+  Check(IsFocused(FfrmCalculaCusto.btnCalcular), ' Teste OK, pois verifica que  de fato o SetFocus nÃ£o estÃ¡ no btnCalcular');
 end;
 
 procedure TestTfrmCalculaCusto.Valida_TabOrder;
 begin
   FFailsOnNoChecksExecuted := false;
   Tab(0);
-  Check(GetFocused = FfrmCalculaCusto.edtQtde_Estoque_Anterior, 'Parametro Tab esperado edtQtde_Estoque_Anterior é 0');
+  Check(GetFocused = FfrmCalculaCusto.edtQtde_Estoque_Anterior, 'Parametro Tab esperado edtQtde_Estoque_Anterior Ã© 0');
   Tab(2);
-  Check(GetFocused = FfrmCalculaCusto.edtCusto_Médio_Anterior,  'Parametro Tab fora do esperado');
+  Check(GetFocused = FfrmCalculaCusto.edtCusto_MÃ©dio_Anterior,  'Parametro Tab fora do esperado');
   //Melhora a rotina de tab
 end;
 
